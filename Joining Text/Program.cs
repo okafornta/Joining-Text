98ca67ce0f2f93cb262611eb4dcbdbd51d46c480
Okafornta <hillaryonia@gmail.com>
2022-11-21 09:57:21 +0100

    Add project files.

---
+++ Joining Text/Program.cs
@@ -0,0 +1,19 @@
+﻿/* TASK: 
+ *
+ *The task is to explore how to add text together 
+ */
+
+// Normal text 
+Console.WriteLine("My Name is Hillary Okafornta");
+
+// Also normal text
+Console.WriteLine(" I graduated from University of Nigeria ");
+
+// Another normal text
+Console.WriteLine(" I am now learning how to code in C#");
+
+// Joining the whole text together
+Console.WriteLine("My Name is Hillary Okafornta," + " I graduated from University of Nigeria, " + " I am now learning how to code in C#");
+
+// Waiting for Enter
+Console.ReadLine();
